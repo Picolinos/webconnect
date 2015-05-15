73e7bbb46ae87d49384218a12e43fbff8e634940
unknown <eduardo.spalvarez@CAS468582J47504.senacsp.edu.br>
2015-05-14 22:03:52 -0300

    entity atualizado! \o

--- projeto/classes/entity.php
+++ projeto/classes/entity.php
@@ -1,15 +1,95 @@
-<?php
-
-include_once 'entity_class.php';  
-class Evento  extends Entity {
+<?   include_once 'entity_class.php'; 
+ 
+class Avalaicao  extends Entity {
+  public $id_avaliacao = null;
+  public $id_usuario = null;
   public $id_evento = null;
-  public $desc_evento = null;
-  public $data_evento = null;
+  public $id_empresa = null;
+  public $valor_avaliacao = null;
+  public $comentario = null;
   public static function getTableName(){ 
-     return  'evento'; 
+     return  'avalaicao'; 
+   }
+  public static function getPrimaryKey(){
+     return   'id_avaliacao'; 
+   }
+  public static function getColumns(){
+   return array_keys(get_class_vars(get_class($this))); 
+   } 
+};  
+class Categorias  extends Entity {
+  public $id_categoria = null;
+  public $nome = null;
+  public static function getTableName(){ 
+     return  'categorias'; 
+   }
+  public static function getPrimaryKey(){
+     return   'id_categoria'; 
+   }
+  public static function getColumns(){
+   return array_keys(get_class_vars(get_class($this))); 
+   } 
+};  
+class Empresa  extends Entity {
+  public $id_empresa = null;
+  public $nome = null;
+  public $coordenada_x = null;
+  public $coordenada_y = null;
+  public $descricao = null;
+  public $telefone = null;
+  public static function getTableName(){ 
+     return  'empresa'; 
+   }
+  public static function getPrimaryKey(){
+     return   'id_empresa'; 
+   }
+  public static function getColumns(){
+   return array_keys(get_class_vars(get_class($this))); 
+   } 
+};  
+class Eventos  extends Entity {
+  public $id_evento = null;
+  public $coordenada_x = null;
+  public $coordenada_y = null;
+  public $Nome = null;
+  public $descricao = null;
+  public $id_empresa = null;
+  public $id_categoria = null;
+  public static function getTableName(){ 
+     return  'eventos'; 
    }
   public static function getPrimaryKey(){
      return   'id_evento'; 
+   }
+  public static function getColumns(){
+   return array_keys(get_class_vars(get_class($this))); 
+   } 
+};  
+class Favoritos  extends Entity {
+  public $id_usuario = null;
+  public $id_evento = null;
+  public $id_empresa = null;
+  public static function getTableName(){ 
+     return  'favoritos'; 
+   }
+  public static function getPrimaryKey(){
+     return   ''; 
+   }
+  public static function getColumns(){
+   return array_keys(get_class_vars(get_class($this))); 
+   } 
+};  
+class Usuarios  extends Entity {
+  public $id_usuario = null;
+  public $nome = null;
+  public $login = null;
+  public $senha = null;
+  public $email = null;
+  public static function getTableName(){ 
+     return  'usuarios'; 
+   }
+  public static function getPrimaryKey(){
+     return   'id_usuario'; 
    }
   public static function getColumns(){
    return array_keys(get_class_vars(get_class($this))); 
