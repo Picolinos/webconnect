b815c1afd0bc226358f3791991272b1bb73e8895
unknown <eduardo.spalvarez@CAS419388D16018.senacsp.edu.br>
2015-05-18 19:58:35 -0300

    ação para ter flow fuido

--- www/js/action.js
+++ www/js/action.js
@@ -1,4 +1,12 @@
-$("a").click(function(){
-    var href = $(this).attr('href');
-    window.location.replace(href);
-})
+$(function(){
+    $("div.import").each(function(){
+        var _self = $(this);
+        var page = _self.attr('id');//+".html";
+        console.log(page);
+        _self.load(page, function(response, status, xhr) {
+            alert(status);
+            alert( "Load was performed." );
+        });//carrega a página referente a esse panel. 
+    });
+   
+});
