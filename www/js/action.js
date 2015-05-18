0c3baba896c2bb4757e696978b1536a792b9d19d
unknown <eduardo.spalvarez@CAS419388D16018.senacsp.edu.br>
2015-05-18 20:12:00 -0300

    alteração e teste de action

--- www/js/action.js
+++ www/js/action.js
@@ -2,7 +2,7 @@ $(function(){
     $("div.import").each(function(){
         var _self = $(this);
         var page = _self.attr('id')+".html";
-        $.get(page);
+        var get = $.get(page);
         var html = get.resposeText;
         _self.html(html);
     });
