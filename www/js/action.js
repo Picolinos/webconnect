424b62dfae216ba044ad72e38400d811f49fcba1
unknown <eduardo.spalvarez@CAS419388D16018.senacsp.edu.br>
2015-05-18 21:21:52 -0300

    alteração de login para main

--- www/js/action.js
+++ www/js/action.js
@@ -3,8 +3,9 @@ $(function(){
         var _self = $(this);
         var page = _self.attr('id')+".html";
         $.get(page, function( data ) {
-          _self.html( data );
+            _self.html( data );
         });
+        
     });
     $(".import").each(function(){
         $(this).click(function(e){
---
+++ www/js/login.js
@@ -0,0 +1,29 @@
+$.ui.autoLaunch = false; 
+$.ui.backButtonText = "Voltar";
+
+$(document).ready(function(){
+    $.ui.launch();
+    // setup signin and signup button events
+    $("#login").on("click", function(){
+        signIn();
+    });
+
+    $("#register").on("click", function(){
+        signUp();
+    });
+});
+function signIn(){
+    var username = $("#username").val();
+    var password = $("#password").val();
+    if(username == 'admin' && password=='admin'){
+        //window.location.replace("home.html");
+        $.ui.backButtonText = "Logout";
+        $.ui.loadContent("home", null, null, "fade");
+         $.ui.backButtonText = "Voltar";
+    }else{
+        $("#error").html("Usuario ou senha invalidos");
+    }
+}
+function signUp(){
+    $.ui.loadContent("main", null, null, "fade");
+}   
