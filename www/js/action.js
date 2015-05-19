94301a32954ab166338d6a5aea9030a95e8215b4
Eduardo Alvarez <eduardo.alvarez1@hotmail.com>
2015-05-19 00:36:04 -0300

    popup funcionando que e uma maravilha e novo style tambem

--- www/js/action.js
+++ www/js/action.js
@@ -9,10 +9,44 @@ $(function(){
         
     });
     setTimeout(function(){
+        
         $(".popup").click(function(){
             var pop = $("#"+$(this).attr('rel'));
+            addOverlay(pop);
+            addBtns(pop);
             pop.show();
         });
     },2000);
+    function addOverlay(pop){
+        var overlay = pop.closest('.panel').find('.overlay');
+        if(overlay.length == 0){
+            pop.closest('.panel').append("<div class='overlay'></div>");
+        }else{
+           overlay.show(); 
+        }
+    }
+    function addBtns(pop){
+        var btns = $("<div class='buttons'></div>");
+        var ok = $("<div class='btn ok'>Ok</div>");
+        var cancelar = $("<div class='btn cancelar'>Cancelar</div>");
+        btns.append(ok);
+        btns.append(cancelar);
+        var close = $("<div class='popup-close'>X</div>")
+        close.click(function(){
+            var content = $(this).closest('.popup-content');
+            content.hide(); 
+            $(this).closest('.panel').find('.overlay').hide();
+        });
+        cancelar.click(function(){
+            close.click();
+        });
+        ok.click(function(){
+            close.click();
+        });
+        if(pop.find('.buttons').length == 0){
+            pop.append(btns);
+            pop.find(".popup-title").append(close)
+        }
+    }
 });
 
