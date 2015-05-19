7452d44a0577b14f7ae12dffe3e5fc8f44aa5fed
unknown <eduardo.spalvarez@CAS419388D16018.senacsp.edu.br>
2015-05-18 21:37:57 -0300

    novas paginas

--- www/js/action.js
+++ www/js/action.js
@@ -1,18 +1,20 @@
 $(function(){
     $("div.import").each(function(){
         var _self = $(this);
+        
         var page = _self.attr('id')+".html";
         $.get(page, function( data ) {
+            console.log(page);
             _self.html( data );
         });
         
     });
-    $(".import").each(function(){
-        $(this).click(function(e){
+    $("a").each(function(){
+        var _self = $(this);
+        _self.click(function(e){
             e.preventDefault();
-            var _self = $(this);
-            var page = _self.rel('rel');
-            $.ui.loadContent(page, null, null, "fade");
+            var page = _self.attr('href');
+            console.log(page);
         });
     });
 });
--- www/js/login.js
+++ www/js/login.js
@@ -19,7 +19,7 @@ function signIn(){
         //window.location.replace("home.html");
         $.ui.backButtonText = "Logout";
         $.ui.loadContent("home", null, null, "fade");
-         $.ui.backButtonText = "Voltar";
+        $.ui.backButtonText = "Voltar";
     }else{
         $("#error").html("Usuario ou senha invalidos");
     }
