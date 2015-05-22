56212cefbc4c713f864899325baa57242c797864
unknown <eduardo.spalvarez@CAS468582J47504.senacsp.edu.br>
2015-05-21 21:37:41 -0300

    include de user business

--- projeto/classes/includes.php
+++ projeto/classes/includes.php
@@ -1,7 +1,7 @@
 <?php 
 require_once('dbManager.php');
 require_once('entity.php');
-require_once('evento_business.php');
+require_once('evento_business.php');require_once('user_business.php');
 
 
 ?>
